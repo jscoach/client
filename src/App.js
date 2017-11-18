09a8d969cd08647608e4a09c83880dd7e52d854b
David Francisco <dmfrancisc@gmail.com>
2017-11-18 16:39:42 +0000

    Initial integration with react-router
    Based on the official example available here: https://goo.gl/LS8fXJ

--- src/App.js
+++ src/App.js
@@ -1,19 +1,62 @@
-import React from "react";
+import React, { Component } from "react";
 import { InstantSearch } from "react-instantsearch/dom";
+import PropTypes from "prop-types";
+import qs from "qs";
 
 import Search from "./Search";
-import logo from './logo.svg';
+import logo from "./logo.svg";
 import "./App.css";
 
-const App = () => (
-  <InstantSearch
-    appId={process.env.REACT_APP_ALGOLIA_APP_ID}
-    apiKey={process.env.REACT_APP_ALGOLIA_API_KEY}
-    indexName={process.env.REACT_APP_INDEX_BY_RELEVANCE}
-  >
-    <img src={logo} className="App-logo" alt="logo" />
-    <Search />
-  </InstantSearch>
-);
+const createURL = state => `?${qs.stringify(state)}`;
+const searchStateToUrl = (props, searchState) =>
+  searchState ? `${props.location.pathname}${createURL(searchState)}` : "";
+const urlToSearchState = location => qs.parse(location.search.slice(1));
+
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = { searchState: urlToSearchState(props.location) };
+  }
+
+  componentWillReceiveProps(props) {
+    if (props.location !== this.props.location) {
+      this.setState({ searchState: urlToSearchState(props.location) });
+    }
+  }
+
+  onSearchStateChange = (searchState) => {
+    clearTimeout(this.debouncedSetState);
+    this.debouncedSetState = setTimeout(() => {
+      this.props.history.push(
+        searchStateToUrl(this.props, searchState),
+        searchState
+      );
+    }, 700);
+    this.setState({ searchState });
+  }
+
+  render() {
+    return (
+      <InstantSearch
+        appId={process.env.REACT_APP_ALGOLIA_APP_ID}
+        apiKey={process.env.REACT_APP_ALGOLIA_API_KEY}
+        indexName={process.env.REACT_APP_INDEX_BY_RELEVANCE}
+        searchState={this.state.searchState}
+        onSearchStateChange={this.onSearchStateChange}
+        createURL={createURL}
+      >
+        <img src={logo} className="App-logo" alt="logo" />
+        <Search />
+      </InstantSearch>
+    );
+  }
+}
+
+App.propTypes = {
+  history: PropTypes.shape({
+    push: PropTypes.func.isRequired
+  }),
+  location: PropTypes.object.isRequired
+};
 
 export default App;
