f84642c64fa1cee898f13879e7852a3a09b3b7ff
David Francisco <dmfrancisc@gmail.com>
2017-11-29 23:21:46 +0000

    Simplify humanized numbers

--- src/humanizedNumber.js
+++ src/humanizedNumber.js
@@ -1,6 +1,6 @@
 import numeral from "numeral";
 
-// This converts a number such as 4200 to 4.2K and 1004 to 1K
-const format = "0[.]0a";
+// This converts a number such as 4200 to 4k
+const format = "0a";
 
 export default value => numeral(value).format(format);
