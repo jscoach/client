3c0569b737c43a009a2cb00cb6c43ca73c42dd7e
Jalal azimi <jalalazimi@gmail.com>
2020-04-19 21:23:07 +0100

    feat: make package page ssr

--- pages/package/[...slug].js
+++ pages/package/[...slug].js
@@ -4,40 +4,65 @@ import algoliasearch from "algoliasearch/lite";
 import Readme from "../../components/Readme";
 import Topbar from "../../components/Topbar";
 import { withRouter } from "next/router";
+import { findResultsState } from "react-instantsearch-dom/server";
+import get from "lodash.get";
+import { ScrollTo } from "react-instantsearch-dom";
 
 const searchClient = algoliasearch(process.env.REACT_APP_ALGOLIA_APP_ID, process.env.REACT_APP_ALGOLIA_API_KEY,);
 
-const LibraryDetails = ({package_name, package_user, router}) => {
+const DEFAULT_PROPS = {
+  searchClient,
+  indexName: process.env.REACT_APP_INDEX_BY_RELEVANCE,
+};
+
+function Algolia(props) {
+  return <InstantSearch
+    searchClient={searchClient}
+    indexName={process.env.REACT_APP_INDEX_BY_RELEVANCE}
+  >
+    <Configure filters={`objectID:${props.id}`}/>
+    <ScrollTo>
+      <Topbar/>
+      <Readme id={props.id}/>
+    </ScrollTo>
+  </InstantSearch>
+}
+
+const LibraryDetails = ({package_name, package_user, router, resultsState}) => {
   let id = router.query.objectID;
   if (!id) {
     id = package_user ? `${package_user}/${package_name}` : package_name;
   }
 
   return <>
-    <InstantSearch
-      searchClient={searchClient}
-      indexName={process.env.REACT_APP_INDEX_BY_RELEVANCE}>
-      <Configure filters={`objectID:${id}`}/>
-      <Topbar/>
-      <Readme id={id}/>
-    </InstantSearch>
+    <Algolia
+      {...DEFAULT_PROPS}
+      resultsState={resultsState}
+      id={id}
+    />
   </>;
 };
 
 LibraryDetails.getInitialProps = async (props) => {
-  const {slug} = props.query;
+  const slug = get(props, 'query.slug');
+  const objectID = get(props, 'query.objectID');
+  const resultsState = await findResultsState(Algolia, {
+    ...DEFAULT_PROPS,
+    id: `objectID:${objectID}`
+  });
 
   if (slug.length > 1) {
     return {
       package_name: slug[1],
-      package_user: slug[0]
+      package_user: slug[0],
+      resultsState
     }
   } else {
     return {
-      package_name: slug[0]
+      package_name: slug[0],
+      resultsState
     }
   }
-
 };
 
 export default withRouter(LibraryDetails);
