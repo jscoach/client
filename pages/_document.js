0dc38ccf20f8f49dac41ea5ef4295de8398f56f9
Jalal azimi <jalalazimi@gmail.com>
2020-04-19 19:47:48 +0100

    feat: add google analytic

--- pages/_document.js
+++ pages/_document.js
@@ -20,19 +20,24 @@ export default class MyDocument extends Document {
         <meta charSet="utf-8"/>
         <meta name="referrer" content="origin-when-cross-origin"/>
         <meta httpEquiv="x-ua-compatible" content="ie=edge"/>
-          <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no"/>
-          <link href="https://fonts.googleapis.com/css?family=Karla:400,700" rel="stylesheet"/>
-          <link rel="shortcut icon" href="/favicon.ico"/>
-          <link rel="manifest" href="/manifest.json"/>
-          <meta name="theme-color" content="#fec93e"/>
-          <meta name="apple-mobile-web-app-status-bar-style" content="#181f2c"/>
-          <script async src="https://www.googletagmanager.com/gtag/js?id=UA-71724704-1"/>
+        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no"/>
+        <link href="https://fonts.googleapis.com/css?family=Karla:400,700" rel="stylesheet"/>
+        <link rel="shortcut icon" href="/favicon.ico"/>
+        <link rel="manifest" href="/manifest.json"/>
+        <meta name="theme-color" content="#fec93e"/>
+        <meta name="apple-mobile-web-app-status-bar-style" content="#181f2c"/>
+        <script async src="https://www.googletagmanager.com/gtag/js?id=UA-71724704-1"/>
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `window.dataLayer = window.dataLayer || [];function gtag(){dataLayer.push(arguments);}gtag('js', new Date());gtag('config', 'UA-71724704-1');`
+          }}
+        />
       </Head>
       <body className="overflow-x-hidden">
       <Main/>
       <NextScript/>
       </body>
       </html>
-  );
+    );
   }
-  }
+}
