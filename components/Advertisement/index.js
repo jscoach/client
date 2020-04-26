02fdbe22eb54429349974c1503cbb30413001995
Jalal azimi <jalalazimi@gmail.com>
2020-04-26 16:48:06 +0100

    fix: enable flag over ad

--- components/Advertisement/index.js
+++ components/Advertisement/index.js
@@ -1,17 +1,19 @@
 import React from "react";
 
-function Index() {
+let initialized = process.env.REACT_APP_SHOW_ADS !== "true";
 
+function Index() {
   React.useEffect(() => {
-    if (window) (window.adsbygoogle = window.adsbygoogle || []).push({});
+    if (window && !initialized) (window.adsbygoogle = window.adsbygoogle || []).push({});
   }, [])
   return (
-    <ins className="adsbygoogle block"
-         data-ad-client="ca-pub-5020228531772566"
-         data-ad-slot="4653679541"
-         data-ad-format="auto"
-         data-full-width-responsive="true"/>
-  );
+    !initialized ?
+      <ins className="adsbygoogle block"
+           data-ad-client="ca-pub-5020228531772566"
+           data-ad-slot="4653679541"
+           data-ad-format="auto"
+           data-full-width-responsive="true"/> : null
+  )
 }
 
 export default Index;
